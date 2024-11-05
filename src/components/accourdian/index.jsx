be94be2696f5992f1303e9330070a4048788b36f
Melkie <melkieyilk@gmail.com>
2024-11-05 10:49:23 +0300

    Load the datas into the page and add listner

---
+++ src/components/accourdian/index.jsx
@@ -0,0 +1,37 @@
+//single selection
+//multiple selection
+import "./styles.css";
+import React, { useState } from "react";
+import data from "./data";
+export default function Accourdian() {
+  const [selected, setSelected] = useState(null);
+
+  return (
+    <div className="wrapper">
+      <div className="accourdian">
+        {data && data.length > 0
+          ? data.map((item) => {
+              return (
+                <div key={item.id} className="item">
+                  <div
+                    className="title"
+                    onClick={() =>
+                      setSelected(selected === item.id ? null : item.id)
+                    }
+                  >
+                    <h3>{item.question}</h3>
+                    <span>{selected === item.id ? "-" : "+"}</span>
+                  </div>
+                  <div
+                    className={`content ${selected === item.id ? "open" : ""}`}
+                  >
+                    <p>{item.answer}</p>
+                  </div>
+                </div>
+              );
+            })
+          : null}
+      </div>
+    </div>
+  );
+}
