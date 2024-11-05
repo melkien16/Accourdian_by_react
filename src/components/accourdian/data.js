330fa8e1be870e9c604108b7dfd199ca08dfa6ab
Melkie <melkieyilk@gmail.com>
2024-11-05 15:54:44 +0300

    Modify the accordians data and some styles

--- src/components/accourdian/data.js
+++ src/components/accourdian/data.js
@@ -3,25 +3,26 @@ const data = [
     id: "1",
     question: "What are accourdian components?",
     answer:
-      "Accourdian components are user interface elements used for organisms to handle another elements",
+      "Accordion components include a container, items, headers/buttons, panels/content, optional icons for state indication, and optional animations for smooth transitions.",
   },
   {
     id: "2",
     question: "What are they used for?",
-    answer: "They are commonly employed in various contexts, including FAQs",
+    answer:
+      "Accordions organize content into collapsible sections, enhancing user experience by saving space and displaying only relevant information when needed, perfect for FAQs and content-heavy pages.",
   },
   {
     id: "3",
     question: "Accourdian is a musical instrument",
     answer:
-      "The accourdian is a musical instrument with a keyboard adn bellow another music flavors",
+      "Yes, an accordion is a musical instrument played by compressing and expanding a bellows while pressing keys or buttons to produce musical notes. It is often associated with folk, classical, and traditional music across various cultures and produces sound by pushing air through reeds.",
   },
   {
     id: "4",
     question:
       "Can I create an accourian component with a different from another components",
     answer:
-      "Yes of course, it is very possible to create as accourdion components",
+      "Yes, you can create a custom accordion component by altering its style, content, behavior, and interaction methods to differentiate it from standard designs.",
   },
 ];
 
