ec8273746e1121b84c28721af0f87f6a21be34e3
Melkie <melkieyilk@gmail.com>
2024-11-05 09:13:51 +0300

    Create a folder called component and add a data file

---
+++ src/components/accourdian/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: "1",
+    question: "What are accourdian components?",
+    answer:
+      "Accourdian components are user interface elements used for organisms to handle another elements",
+  },
+  {
+    id: "2",
+    question: "What are they used for?",
+    answer: "They are commonly employed in various contexts, including FAQs",
+  },
+  {
+    id: "3",
+    question: "Accourdian is a musical instrument",
+    answer:
+      "The accourdian is a musical instrument with a keyboard adn bellow another music flavors",
+  },
+  {
+    id: "4",
+    question:
+      "Can I create an accourian component with a different from another components",
+    answer:
+      "Yes of course, it is very possible to create as accourdion components",
+  },
+];
+
+export default data;
